c9849a35da764805e5e97c8b0e73af8d388222af
Kevin Ramos <kevin.jo.ramos@gmail.com>
2026-09-23 10:09:08 -0700

    I've decided to overhaul how I'm working with binary numbers for this project. It's just easier to work with I nt rather than kotlin's unsigned and byte types. Any data in storage mechanisms will still be in byte for the sake of size, but that data will be inflated to int during it's use.

--- common/src/main/kotlin/kevtendo/common/util/Binary.kt
+++ common/src/main/kotlin/kevtendo/common/util/Binary.kt
@@ -1,2 +1,17 @@
 package kevtendo.common.util
 
+fun Int.maskTo8Bits() = this and 0xFF
+fun Int.maskTo16Bits() = this and 0xFFFF
+
+fun Int.getBit(bit: Int): Boolean {
+    require(bit in 0..15) { "Bit index out of range: $bit" }
+    return (this and (1 shl bit)) > 0
+}
+
+fun Int.withBit(bit: Int, isSet: Boolean): Int {
+    require(bit in 0..15) { "Bit index out of range: $bit" }
+    if (isSet) return this or (1 shl bit)
+    return this and (1 shl bit).inv()
+}
+
+fun formWord(low: Int, high: Int): Int = low or (high shl 8)
